6f681d4a46adc59b04925ea690495e57f6b5290d
Sachin Parmar <sachin.parmar@Borosil.com>
2024-04-27 17:16:27 +0530

    Second Step

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  final int days = 30;
+  @override
+
+  Widget build(BuildContext context) {
+  return Scaffold(
+        appBar: AppBar(
+          title: Text("My App"),
+        ),
+        body: Center(
+          child: Container(
+            child: Text("Its Sachin Parmar $days"),
+          ),
+        ),
+        drawer: Drawer(),
+      );
+  }
+}
