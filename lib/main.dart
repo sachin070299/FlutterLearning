405f89aaf1ce927f7077969f73bf56972cf1986d
SACHIN PARMAR <sachin070299@gmail.com>
2024-04-29 11:17:00 +0530

    Third Step

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 // ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors, use_super_parameters
 
+import 'package:firstapp/screens/home_page.dart';
+import 'package:firstapp/screens/login_page.dart';
 import 'package:flutter/material.dart';
-import 'home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,6 +14,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes:{
+        "/":(context)=>LoginPage(),
+       "/home":(context)=>HomePage(),
+       "/login":(context)=>LoginPage(),
+      }
+      );
   }
 }
---
+++ lib/screens/login_page.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget{
+  @override
+
+  Widget build(BuildContext context)
+  {
+    return Material(
+      child:Center(
+      child:Text("LoginPage"),
+    ),
+    );
+  }
+}
